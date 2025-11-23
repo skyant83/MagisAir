176f26fdc0ef9bf8b3a5d0ab6d6ae352dad62cd4
skyant83 <skyant83@gmail.com>
2025-11-23 21:11:30 +0800

    Simple Refactoring

--- core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.booking_view, name='booking_view'),
-    path('success/', views.success_view, name='success_view'),
-    path('passengers/', views.passenger_list_view, name='passenger_list'), # <--- Add this
-]
